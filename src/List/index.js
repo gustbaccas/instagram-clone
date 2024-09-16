e653c954fdfb99c07478a2f64366b0079685f40c
gustbaccas <gustbaccas@outlook.com>
2024-09-16 18:47:01 -0300

    adicionei as fotos do feed

--- src/List/index.js
+++ src/List/index.js
@@ -1,10 +1,44 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 
-export default function List() {
-    return(
+export default function List(props) {
+    return (
         <View>
-            <Text>Lista Feed</Text>
+            <View style={styles.viewPerfil}>
+                <Image
+                    source={{ uri: props.data.imgperfil }}
+                    style={styles.fotoPerfil}
+                />
+                <Text style={styles.nomeUsuario}>{props.data.nome}</Text>
+            </View>
+
+            <Image
+            source={{ uri: props.data.imgPublicacao}}
+            style={styles.fotoPublicacao}
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    viewPerfil: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center',
+        padding: 8
+    },
+    fotoPerfil: {
+        width: 50,
+        height: 50,
+        borderRadius: 25
+    },
+    nomeUsuario: {
+        paddingLeft: 5,
+        fontSize: 22,
+        color: '#000'
+    },
+    fotoPublicacao: {
+        width: '100%',
+        height: 350
+    }
+})
