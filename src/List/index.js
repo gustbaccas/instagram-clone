7bd49e9305106641a5368750885540bf14635ee9
gustbaccas <gustbaccas@outlook.com>
2024-09-18 21:00:35 -0300

    criei uma função de likes e adicionei os icons da foto do feed

--- src/List/index.js
+++ src/List/index.js
@@ -1,7 +1,13 @@
 import React from "react";
-import { Text, View, StyleSheet, Image } from "react-native";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
 
 export default function List(props) {
+
+    function carregaIcone(likeada){
+        return likeada ? require('../img/Icon6.jpeg') :
+        require('../img/Icon2.jpeg')
+    }
+
     return (
         <View>
             <View style={styles.viewPerfil}>
@@ -13,9 +19,35 @@ export default function List(props) {
             </View>
 
             <Image
+            resizeMode="cover"
             source={{ uri: props.data.imgPublicacao}}
             style={styles.fotoPublicacao}
             />
+
+            <View style={styles.Icons}>
+                <TouchableOpacity>
+                    <Image
+                    source={carregaIcone(props.data.likeada)}
+                    style={styles.iconeLike}
+                    />
+                </TouchableOpacity>
+                
+                <TouchableOpacity>
+                    <Image
+                    source={require('../img/Icon3.jpeg')}
+                    style={styles.iconeComent}
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Image
+                    source={require('../img/Icon1.jpeg')}
+                    style={styles.iconeSend}
+                    />
+                </TouchableOpacity>
+            </View>
+
+
         </View>
     )
 }
@@ -38,7 +70,26 @@ const styles = StyleSheet.create({
         color: '#000'
     },
     fotoPublicacao: {
-        width: '100%',
-        height: 350
+        height: 350,
+        alignItems: 'center'
+    },
+    Icons: {
+        flexDirection: 'row',
+        padding: 5,
+        paddingLeft: 15
+    },
+    iconeLike: {
+        width: 26,
+        height: 26,
+        marginRight: 15
+    },
+    iconeComent: {
+        height: 26,
+        width: 26,
+        marginRight: 15
+    },
+    iconeSend: {
+        width: 26,
+        height: 26,
     }
 })
